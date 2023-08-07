31b229e7bfc614c79aab87aa3b259df142e68d21
Vinícius Pólvora <santosvinepolvora@gmail.com>
2023-08-07 20:58:17 -0300

    implementa fluxo repository - get

--- src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncionarioListResponse.java
+++ src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncionarioListResponse.java
@@ -1,7 +1,9 @@
 package br.com.santander.funcionarios.funcionarios.application.api;
 
+import br.com.santander.funcionarios.funcionarios.domain.Funcionario;
 import lombok.Value;
 
+import java.util.List;
 import java.util.UUID;
 @Value
 public class FuncionarioListResponse {
@@ -9,4 +11,8 @@ public class FuncionarioListResponse {
     private String nomeCompleto;
     private String cpf;
     private String email;
+
+    public static List<FuncionarioListResponse> converte(List<Funcionario> funcionarios) {
+        return null;
+    }
 }
--- src/main/java/br/com/santander/funcionarios/funcionarios/application/repository/FuncionarioRepository.java
+++ src/main/java/br/com/santander/funcionarios/funcionarios/application/repository/FuncionarioRepository.java
@@ -2,6 +2,9 @@ package br.com.santander.funcionarios.funcionarios.application.repository;
 
 import br.com.santander.funcionarios.funcionarios.domain.Funcionario;
 
+import java.util.List;
+
 public interface FuncionarioRepository {
     Funcionario salva(Funcionario funcionario);
+    List<Funcionario> buscaTodosFuncionarios();
 }
