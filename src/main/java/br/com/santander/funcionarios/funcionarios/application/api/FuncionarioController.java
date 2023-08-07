a7856631b9de278377e55a1b59e51f12581275ec
Vinícius Pólvora <santosvinepolvora@gmail.com>
2023-08-07 16:39:33 -0300

    implementa controlle e fluxo para service

--- src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncionarioController.java
+++ src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncionarioController.java
@@ -1,14 +1,18 @@
 package br.com.santander.funcionarios.funcionarios.application.api;
 
+import br.com.santander.funcionarios.funcionarios.application.service.FuncionarioService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Log4j2
 public class FuncionarioController implements FuncionarioApi {
+    private final FuncionarioService funcionarioService;
+
     @Override
     public FuncionarioResponse postFuncionario(FuncinarioRequest funcinarioRequest) {
         log.info("[inicia] FuncionarioController - postFuncionario");
+        FuncionarioResponse funcionarioCriado = funcionarioService.criaFuncionario(funcinarioRequest)
         log.info("[finaliza ] FuncionarioController - postFuncionario");
         return null;
     }
---
+++ src/main/java/br/com/santander/funcionarios/funcionarios/application/service/FuncionarioService.java
@@ -0,0 +1,4 @@
+package br.com.santander.funcionarios.funcionarios.application.service;
+
+public class FuncionarioService {
+}
