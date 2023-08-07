1e5695a73b9a67fc9c72b1e766aa005531d21449
Vinícius Pólvora <santosvinepolvora@gmail.com>
2023-08-07 16:47:40 -0300

    implementa request e respose

--- src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncinarioRequest.java
+++ src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncinarioRequest.java
@@ -1,4 +1,34 @@
 package br.com.santander.funcionarios.funcionarios.application.api;
 
+import br.com.santander.funcionarios.funcionarios.domain.EstadoCivil;
+import br.com.santander.funcionarios.funcionarios.domain.Sexo;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.UUID;
+@Value
 public class FuncinarioRequest {
+
+    @NotNull
+    private String nomeCliente;
+    @CPF
+    private String cpf;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String telefone;
+    @NotBlank
+    private String endereco;
+    @NotNull
+    private LocalDate dataNascimento;
+    private Sexo sexo;
+    private EstadoCivil estadoCivil;
 }
--- src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncionarioResponse.java
+++ src/main/java/br/com/santander/funcionarios/funcionarios/application/api/FuncionarioResponse.java
@@ -1,4 +1,14 @@
 package br.com.santander.funcionarios.funcionarios.application.api;
 
+import lombok.Builder;
+import lombok.Value;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.UUID;
+@Value
+@Builder
 public class FuncionarioResponse {
+    private UUID idFuncionario;
 }
